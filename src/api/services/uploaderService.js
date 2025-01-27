dba766e61f5559c53d15697d56389939944945a5
Dilip Soni <dilipsoni37@gmail.com>
2025-01-27 19:50:07 +0530

    pushed

--- src/api/services/uploaderService.js
+++ src/api/services/uploaderService.js
@@ -1,10 +1,10 @@
-const {
-  GetObjectCommand,
-  PutObjectCommand,
-  DeleteObjectCommand,
-} = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { s3Client } = require("../../config/s3Client");
+// const {
+//   GetObjectCommand,
+//   PutObjectCommand,
+//   DeleteObjectCommand,
+// } = require("@aws-sdk/client-s3");
+// const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+// const { s3Client } = require("../../config/s3Client");
 const BUCKET_NAME = process.env.AWS_S3_BUCKET;
 const CDNURL = process.env.CDN_URL;
 const path = require("path");
@@ -66,114 +66,114 @@ module.exports = {
     // console.log('1212 ',base64, userId, folderName)
     // Ensure that you POST a base64 data to your server.
     // Let's assume the variable "base64" is one.
-    if (base64 && base64.includes("base64")) {
-      base64Data = new Buffer.from(
-        base64.replace(/^data:image\/\w+;base64,/, ""),
-        "base64"
-      );
-    } else {
-      base64Data = new Buffer.from(base64);
-    }
+    // if (base64 && base64.includes("base64")) {
+    //   base64Data = new Buffer.from(
+    //     base64.replace(/^data:image\/\w+;base64,/, ""),
+    //     "base64"
+    //   );
+    // } else {
+    //   base64Data = new Buffer.from(base64);
+    // }
 
-    // Getting the file type, ie: jpeg, png or gif
-    const type = "png"; // base64.split(';')[0].split('/')[1];
-    const bucketParams = {
-      Bucket: `${BUCKET_NAME}`, // drivers/Profiles
-      Key: `${folderName}/${filename}.${type}`, // type is not required
-      Body: base64Data,
-      ACL: "public-read",
-      ContentEncoding: "base64", // required
-      ContentType: `image/${type}`, // required. Notice the back ticks
-    };
+    // // Getting the file type, ie: jpeg, png or gif
+    // const type = "png"; // base64.split(';')[0].split('/')[1];
+    // const bucketParams = {
+    //   Bucket: `${BUCKET_NAME}`, // drivers/Profiles
+    //   Key: `${folderName}/${filename}.${type}`, // type is not required
+    //   Body: base64Data,
+    //   ACL: "public-read",
+    //   ContentEncoding: "base64", // required
+    //   ContentType: `image/${type}`, // required. Notice the back ticks
+    // };
 
-    let location = "";
-    let key = "";
-    try {
-      await s3Client.send(new PutObjectCommand(bucketParams)); // await s3Client.upload(params).promise();
-      const cdnURL = `${CDNURL}/${folderName}/${filename}.${type}`;
-      //const cdnURL = response.$metadata.httpHeaders['x-amz-cf-id'];
+    // let location = "";
+    // let key = "";
+    // try {
+    //   await s3Client.send(new PutObjectCommand(bucketParams)); // await s3Client.upload(params).promise();
+    //   const cdnURL = `${CDNURL}/${folderName}/${filename}.${type}`;
+    //   //const cdnURL = response.$metadata.httpHeaders['x-amz-cf-id'];
 
-      // location = Location;
-      // key = Key;
-      //   console.log('location',location,key)
-      return cdnURL;
-    } catch (error) {
-      console.log("s3 error", error);
-    }
+    //   // location = Location;
+    //   // key = Key;
+    //   //   console.log('location',location,key)
+    //   return cdnURL;
+    // } catch (error) {
+    //   console.log("s3 error", error);
+    // }
 
-    // Save the Location (url) to your database and Key if needs be.
-    // As good developers, we should return the url and let other function do the saving to database etc
-    // console.log(location, key);
+    // // Save the Location (url) to your database and Key if needs be.
+    // // As good developers, we should return the url and let other function do the saving to database etc
+    // // console.log(location, key);
 
-    return location;
+    // return location;
   },
   fileUpload: async (fileData, filename, folderName) => {
     // upload file the digial ocean spaces
-    try {
-      const fileExt = fileData.name.split(".").pop();
-      const bucketParams = {
-        Bucket: `${BUCKET_NAME}`, // drivers/Profiles
-        ContentType: fileData.mimetype,
-        Key: `${folderName}/${filename}.${fileExt}`, // type is not required
-        Body: fileData.data,
-        ACL: "public-read",
-      };
+    // try {
+    //   const fileExt = fileData.name.split(".").pop();
+    //   const bucketParams = {
+    //     Bucket: `${BUCKET_NAME}`, // drivers/Profiles
+    //     ContentType: fileData.mimetype,
+    //     Key: `${folderName}/${filename}.${fileExt}`, // type is not required
+    //     Body: fileData.data,
+    //     ACL: "public-read",
+    //   };
 
-      await s3Client.send(new PutObjectCommand(bucketParams)); // await s3Client.upload(params).promise();
-      const cdnURL = `${CDNURL}/${folderName}/${filename}.${fileExt}`;
-      return cdnURL;
-    } catch (error) {
-      console.log("s3 error", error);
-    }
+    //   await s3Client.send(new PutObjectCommand(bucketParams)); // await s3Client.upload(params).promise();
+    //   const cdnURL = `${CDNURL}/${folderName}/${filename}.${fileExt}`;
+    //   return cdnURL;
+    // } catch (error) {
+    //   console.log("s3 error", error);
+    // }
   },
   imageDelete: async (imageName, folderName) => {
-    try {
-      let key = "";
-      let bucketParams = {};
-      const regex =
-        /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-      if (!regex.test(imageName)) {
-        key = imageName;
-        bucketParams = {
-          Bucket: `${BUCKET_NAME}`,
-          Key: `${folderName}/${key}`,
-        };
-      } else {
-        const parsedUrl = require("url").parse(imageName);
-        key = parsedUrl.pathname.substring(1).split("/");
-        console.log(" key ", key);
-        bucketParams = {
-          Bucket: `${BUCKET_NAME}`,
-          Key: `${folderName}/${key[2]}`,
-        };
-      }
+    // try {
+    //   let key = "";
+    //   let bucketParams = {};
+    //   const regex =
+    //     /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    //   if (!regex.test(imageName)) {
+    //     key = imageName;
+    //     bucketParams = {
+    //       Bucket: `${BUCKET_NAME}`,
+    //       Key: `${folderName}/${key}`,
+    //     };
+    //   } else {
+    //     const parsedUrl = require("url").parse(imageName);
+    //     key = parsedUrl.pathname.substring(1).split("/");
+    //     console.log(" key ", key);
+    //     bucketParams = {
+    //       Bucket: `${BUCKET_NAME}`,
+    //       Key: `${folderName}/${key[2]}`,
+    //     };
+    //   }
 
-      const deleteObject = await s3Client.send(
-        new DeleteObjectCommand(bucketParams)
-      );
-      //   const deleteObject = await s3Client.deleteObject(params).promise();
-      return deleteObject;
-    } catch (err) {
-      console.log("errr12", err);
-      return err;
-    }
+    //   const deleteObject = await s3Client.send(
+    //     new DeleteObjectCommand(bucketParams)
+    //   );
+    //   //   const deleteObject = await s3Client.deleteObject(params).promise();
+    //   return deleteObject;
+    // } catch (err) {
+    //   console.log("errr12", err);
+    //   return err;
+    // }
   },
   resizeUpload: async (isProfile, base64Data, width, height) => {
-    try {
-      if (isProfile) {
-        const resizeImage = await sharp(Buffer.from(base64Data, "base64"))
-          .resize(width, height)
-          .sharpen()
-          .toBuffer();
-        return resizeImage;
-      }
-      const resizeImage = await sharp(
-        Buffer.from(base64Data, "base64")
-      ).toBuffer();
-      return resizeImage;
-    } catch (err) {
-      console.log("errr12", err);
-      return err;
-    }
+    // try {
+    //   if (isProfile) {
+    //     const resizeImage = await sharp(Buffer.from(base64Data, "base64"))
+    //       .resize(width, height)
+    //       .sharpen()
+    //       .toBuffer();
+    //     return resizeImage;
+    //   }
+    //   const resizeImage = await sharp(
+    //     Buffer.from(base64Data, "base64")
+    //   ).toBuffer();
+    //   return resizeImage;
+    // } catch (err) {
+    //   console.log("errr12", err);
+    //   return err;
+    // }
   },
 };
--- src/config/s3.js
+++ src/config/s3.js
@@ -1,7 +1,5 @@
 // You can either "yarn add aws-sdk" or "npm i aws-sdk"
 
-const { S3Client, AbortMultipartUploadCommand } = require("@aws-sdk/client-s3");
-
 const BUCKET_NAME = process.env.AWS_S3_BUCKET;
 
 // Configure AWS to use promise
@@ -13,15 +11,7 @@ const BUCKET_NAME = process.env.AWS_S3_BUCKET;
 // });
 
 // Create an s3 instance
-const s3Client = new S3Client({
-  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
-  endpoint: "https://nyc3.digitaloceanspaces.com",
-  region: process.env.AWS_DEFAULT_REGION, //"us-east-1",
-  credentials: {
-    accessKeyId: process.env.SPACES_KEY,
-    secretAccessKey: process.env.SPACES_SECRET
-  }
-});
+const s3Client = {};
 
 /**
  *
@@ -33,34 +23,34 @@ module.exports = {
         // console.log('1212 ',base64, userId, folderName)
         // Ensure that you POST a base64 data to your server.
         // Let's assume the variable "base64" is one.
-        const base64Data = new Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+        // const base64Data = new Buffer.from(base64.replace(/^data:image\/\w+;base64,/, ''), 'base64');
 
-        // Getting the file type, ie: jpeg, png or gif
-        const type = 'png'; // base64.split(';')[0].split('/')[1];
-        const params = {
-            Bucket: `${BUCKET_NAME}/${folderName}`, // drivers/Profiles
-            Key: `${userId}.${type}`, // type is not required
-            Body: base64Data,
-            ACL: 'public-read',
-            ContentEncoding: 'base64', // required
-            ContentType: `image/${type}`, // required. Notice the back ticks
-        };
+        // // Getting the file type, ie: jpeg, png or gif
+        // const type = 'png'; // base64.split(';')[0].split('/')[1];
+        // const params = {
+        //     Bucket: `${BUCKET_NAME}/${folderName}`, // drivers/Profiles
+        //     Key: `${userId}.${type}`, // type is not required
+        //     Body: base64Data,
+        //     ACL: 'public-read',
+        //     ContentEncoding: 'base64', // required
+        //     ContentType: `image/${type}`, // required. Notice the back ticks
+        // };
 
 
-        let location = '';
-        let key = '';
-        try {
-            const {
-                Location,
-                Key,
-            } = await s3Client.upload(params).promise();
-            location = Location;
-            key = Key;
-            //   console.log('location',location,key)
-            return location;
-        } catch (error) {
-            console.log('s3 error', error)
-        }
+        // let location = '';
+        // let key = '';
+        // try {
+        //     const {
+        //         Location,
+        //         Key,
+        //     } = await s3Client.upload(params).promise();
+        //     location = Location;
+        //     key = Key;
+        //     //   console.log('location',location,key)
+        //     return location;
+        // } catch (error) {
+        //     console.log('s3 error', error)
+        // }
 
         // Save the Location (url) to your database and Key if needs be.
         // As good developers, we should return the url and let other function do the saving to database etc
